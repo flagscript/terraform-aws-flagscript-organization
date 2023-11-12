8e35ad995cb3d3f204693cb7402d0692cd6325d2
flagscript <flagscript@gmail.com>
2023-11-12 10:12:30 -0700

    fix(14): Remove params for app setup tags

--- modules/account-setup/variables.tf
+++ modules/account-setup/variables.tf
@@ -1,15 +1,5 @@
 # REQUIRED PARAMETERS
 # These parameters must be supplied when consuming this module.
-variable "application_id" {
-  description = "Application Id"
-  type        = string
-}
-
-variable "client_id" {
-  description = "The id for the client."
-  type        = string
-}
-
 variable "name" {
   description = "The name for the aws account."
   type        = string
@@ -17,8 +7,3 @@ variable "name" {
 
 # OPTIONAL PARAMETERS
 # These parameters have reasonable defaults.
-variable "org_owner" {
-  default     = "flagscript"
-  description = "Owner moniker for the flagscript organization."
-  type        = string
-}
