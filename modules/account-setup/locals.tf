81fb784d3f7dd6aa8876b604c6e8b081867acf14
flagscript <flagscript@gmail.com>
2023-11-12 09:39:55 -0700

    fix(14): Remove forced tags from account-setup module.

--- modules/account-setup/locals.tf
+++ modules/account-setup/locals.tf
@@ -1,12 +1,6 @@
 locals {
   common_tags = {
-    "flagscript:account-owner:owner"            = var.client_id
-    "flagscript:automation:environment"         = "root"
-    "flagscript:cost-allocation:application-id" = var.application_id
-    "flagscript:cost-allocation:owner"          = var.client_id
-    "flagscript:data-owner:owner"               = var.client_id
-    "flagscript:operations:owner"               = var.org_owner
-    "github:module:repository"                  = "flagscript/terraform-aws-flagscript-organization"
-    "terraform:module"                          = "terraform-aws-flagscript-organization//account-setup"
+    "github:module:repository" = "flagscript/terraform-aws-flagscript-organization"
+    "terraform:module"         = "terraform-aws-flagscript-organization//account-setup"
   }
 }
